7448450a4a2cafc6883720021198ec4abc612632
Hanif-Niomic <m.alihanif249@gmail.com>
2022-04-04 21:51:29 +0700

    tugas java
    tugas java

---
+++ tugas27.js
@@ -0,0 +1,17 @@
+function tugas27(){
+  var kata1 = "No";
+  var kata2 = "rumah saya";
+  var kata3 = 30;
+  var kata4 =  "A";
+  var kataa = "Jalan pangeran Tirtayasa No";
+  var katab = 70;
+
+  var gabung1 = kata1 + " " + kata2 + " " + String(kata3) + kata4
+  var gabung2 = kataa + " " + String(katab);
+
+  console.log(gabung1);
+  console.log(gabung2);
+
+}
+
+tugas27()
